a2c863fa93bce9406c1957ec329a563b61307a3b
Jens Keiner <jens.keiner@gmail.com>
2023-08-17 13:33:08 +0200

    Fix version detection.

--- exchange_calendars_extensions/api/__init__.py
+++ exchange_calendars_extensions/api/__init__.py
@@ -2,17 +2,17 @@ try:
     from importlib.metadata import version
     # get version from installed package
     __version__ = version("exchange_calendars_extensions")
+    del version
 except ImportError:
     pass
 
 if __version__ is None:
     try:
         # if package not installed, get version as set when package built.
-        from ._version import version
+        from .version import version
     except Exception:
         # If package not installed and not built, leave __version__ as None
         pass
     else:
         __version__ = version
-
-del version
+        del version
